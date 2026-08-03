976f1e0e46ae77526a00c06a16c35ff6c0a45348
SumonaYeasmin <SumonaYeasmin@users.noreply.github.com>
2026-08-03 15:23:56 +0600

    feat: implement delivery history dashboard with filtering and export capabilities for riders

---
+++ src/app/(dashboardLayout)/rider/history/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+import DeliveryHistoryContent from "@/components/dashboard/rider/deliveryHistory/DeliveryHistoryContent";
+
+export default function DeliveryHistoryPage() {
+  return <DeliveryHistoryContent />;
+}
